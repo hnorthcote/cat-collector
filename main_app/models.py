125104db04b240010a958b1df737497e55f4e8ec
myDeveloperJourney <daniel@mydeveloperjourney.com>
2020-09-11 13:57:06 -0500

    adds latest updates

--- main_app/models.py
+++ main_app/models.py
@@ -1,6 +1,13 @@
 from django.db import models
 from django.urls import reverse
 # Create your models here.
+from datetime import date
+
+MEALS = (
+    ('B', 'Breakfast'),
+    ('L', 'Lunch'),
+    ('D', 'Dinner'),
+)
 
 class Toy(models.Model):
     name = models.CharField(max_length=50)
@@ -8,15 +15,46 @@ class Toy(models.Model):
 
     def get_absolute_url(self):
         return reverse('toy_detail', kwargs={'pk': self.id })
+    
+    def __str__(self):
+        return f"A {self.color} {self.name}"
 
 class Cat(models.Model):
     name = models.CharField(max_length=100)
     breed = models.CharField(max_length=100)
     description = models.TextField(max_length=250)
     age = models.IntegerField()
+    toys = models.ManyToManyField(Toy)
 
     def __str__(self):
         return self.name
     
     def get_absolute_url(self):
         return reverse('detail', kwargs={'cat_id': self.id })
+    
+    def fed_for_today(self):
+        return self.feeding_set.filter(date=date.today()).count() >= len(MEALS)
+
+
+class Feeding(models.Model):
+    date = models.DateField('feeding date')
+    meal = models.CharField(
+        max_length=1,
+        choices=MEALS,
+        default=MEALS[0][0]
+    )
+
+    cat = models.ForeignKey(Cat, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.get_meal_display()} on {self.date}'
+    
+    class Meta:
+        ordering = ['-date']
+
+class Photo(models.Model):
+    url = models.CharField(max_length=200)
+    cat = models.ForeignKey(Cat, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"Photo for cat_id: {self.cat_id} @{self.url}"
--- main_app/views.py
+++ main_app/views.py
@@ -1,9 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.views.generic  import DetailView, ListView
 
-from .models import Cat, Toy
+import uuid
+import boto3
+
+from .models import Cat, Toy, Photo
+from .forms import FeedingForm
+
+
+S3_BASE_URL = 'https://s3-us-west-1.amazonaws.com/'
+BUCKET = 'catcollector-seir-629'
 
 def home(request):
     return render(request, 'home.html')
@@ -16,12 +24,70 @@ def cats_index(request):
     return render(request, 'cats/index.html', {'cats': cats})
 
 def cats_detail(request, cat_id):
+    
     cat = Cat.objects.get(id=cat_id)
-    return render(request, 'cats/detail.html', {'cat': cat})
+    feeding_form = FeedingForm()
+    toys_cat_doesnt_have = Toy.objects.exclude(id__in=cat.toys.all().values_list('id'))
+
+    return render(request, 'cats/detail.html', {
+        'cat': cat,
+        'feeding_form': feeding_form,
+        'toys': toys_cat_doesnt_have
+    })
+
+def add_feeding(request, cat_id):
+    form = FeedingForm(request.POST)
+
+    if form.is_valid():
+        new_feeding = form.save(commit=False)
+        new_feeding.cat_id = cat_id
+        new_feeding.save()
+    
+    return redirect('detail', cat_id=cat_id)
+
+def add_photo(request, cat_id):
+    # Collect the photo asset from the request
+    photo_file = request.FILES.get('photo-file', None)
+
+    # check if a photo asset was provided
+    if photo_file:
+        # make the s3 utility available locally to this view function
+        s3 = boto3.client('s3')
+        # create a key that will be used to add a unique identifier to our photo asset
+        key = uuid.uuid4().hex[:6] + photo_file.name[photo_file.name.rfind('.'):]
+
+        try:
+            # attempt to upload the file to aws
+            s3.upload_fileobj(photo_file, BUCKET, key)
+            #create the unique photo asset url
+            url = f"{S3_BASE_URL}{BUCKET}/{key}"
+            # create an instance the photo model in memory
+            photo = Photo(url=url, cat_id=cat_id)
+            # save the photo model
+            photo.save()
+
+        except:
+            # something went wrong - gracefully do something else
+            print('An error occurred uploading file to s3')
+    
+    return redirect('detail', cat_id=cat_id)
+
+
+
+
+def assoc_toy(request, cat_id, toy_id):
+    Cat.objects.get(id=cat_id).toys.add(toy_id)
+    return redirect('detail', cat_id=cat_id)
+
+
+def unassoc_toy(request, cat_id, toy_id):
+    Cat.objects.get(id=cat_id).toys.remove(toy_id)
+    return redirect('detail', cat_id=cat_id)
+    
 
 class CatCreate(CreateView):
     model = Cat
-    fields = '__all__'
+    fields = ['name', 'breed', 'description', 'age']
 
 class CatUpdate(UpdateView):
     model = Cat
